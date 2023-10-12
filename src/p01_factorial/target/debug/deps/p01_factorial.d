4174db7cfebca1f242ab997df98f123cb3ecf6e9
Yuko Goto <pocket_vcalneiatry@yahoo.co.jp>
2023-10-12 22:14:10 +0900

    t

--- src/p01_factorial/target/debug/deps/p01_factorial.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sasih\desktop\tech\rust\rust_practice\src\p01_factorial\target\debug\deps\p01_factorial.exe: src\main.rs
-
-C:\Users\sasih\desktop\tech\rust\rust_practice\src\p01_factorial\target\debug\deps\p01_factorial.d: src\main.rs
-
-src\main.rs:
--- src/p01_factorial/target/debug/p01_factorial.d
+++
@@ -1,1 +0,0 @@
-C:\Users\sasih\desktop\tech\rust\rust_practice\src\p01_factorial\target\debug\p01_factorial.exe: C:\Users\sasih\desktop\tech\rust\rust_practice\src\p01_factorial\src\main.rs
