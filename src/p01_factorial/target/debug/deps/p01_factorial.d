a8d4acbcfe0ebe7140897ce2fdb5a1ffda73b234
Yuko Goto <pocket_vcalneiatry@yahoo.co.jp>
2023-10-12 22:12:40 +0900

    test commit

---
+++ src/p01_factorial/target/debug/deps/p01_factorial.d
@@ -0,0 +1,5 @@
+C:\Users\sasih\desktop\tech\rust\rust_practice\src\p01_factorial\target\debug\deps\p01_factorial.exe: src\main.rs
+
+C:\Users\sasih\desktop\tech\rust\rust_practice\src\p01_factorial\target\debug\deps\p01_factorial.d: src\main.rs
+
+src\main.rs:
