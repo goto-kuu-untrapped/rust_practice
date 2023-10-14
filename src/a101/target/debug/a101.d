d8cad66fa27c55a230f1d22c182d871a68152adf
Yuko Goto <pocket_vcalneiatry@yahoo.co.jp>
2023-10-14 18:18:52 +0900

    clean repo

--- src/a101/target/debug/a101.d
+++
@@ -1,1 +0,0 @@
-C:\Users\sasih\Desktop\TECH\Rust\rust_practice\src\a101\target\debug\a101.exe: C:\Users\sasih\Desktop\TECH\Rust\rust_practice\src\a101\src\main.rs
--- src/a101/target/debug/deps/a101.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sasih\desktop\tech\rust\rust_practice\src\a101\target\debug\deps\a101.exe: src\main.rs
-
-C:\Users\sasih\desktop\tech\rust\rust_practice\src\a101\target\debug\deps\a101.d: src\main.rs
-
-src\main.rs:
