080d97c70deb4196d0bb1a5006d93b06734734b1
Yuko Goto <pocket_vcalneiatry@yahoo.co.jp>
2023-10-14 23:17:06 +0900

    update

--- src/a107/src/main.rs
+++ src/a107/src/main.rs
@@ -1,25 +1,26 @@
 use std::iter;
 
 fn main() {
+    // print the title
     println!("*** Multiplication for numbers up to 12 ***");
+
     let upper = 12;
-    print!("|multiple");
-    for n in 1..=upper {
-        print!("| {n:3} ");
-    } 
-    println!("|");
+    // print the header row
+    println!("| Multiple | {} |", 
+              (1..=upper)
+              .map(|n| format!("{:5}", n))
+              .collect::<String>());
 
-    for _ in 1..=20 {
-        print!("====");
-    } 
-    println!("");
+    // print the separator row
+    println!("| -------- | {} |", iter::repeat("-").take(5*upper).collect::<String>());
 
+    // Print the multiplication table
     for n in 1..=upper {
-        print!("| {n:5} |");
-        for m in 1..=upper {
-            print!("| {:3} ", n * m);
-        }
-        println!("|");
+        let row: String = (1..=upper)
+            .map(|m| format!("{:5}", n * m))
+            .collect();
+
+        println!("|  {:7} | {} |", n, row);
     }
 
 }
